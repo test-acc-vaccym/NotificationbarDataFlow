bac001567610774ddf9b3f649b4bfd526dfc4487
赵状 <zhaozhuang.cn@gmail.com>
2017-07-13 19:25:51 +0800

    更换图标

--- app/src/main/java/win/jackdaw/notificationbardataflow/MainService.kt
+++ app/src/main/java/win/jackdaw/notificationbardataflow/MainService.kt
@@ -1,7 +1,6 @@
 package win.jackdaw.notificationbardataflow
 
 import android.content.Context
-import android.graphics.drawable.Icon
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import android.telephony.TelephonyManager
@@ -20,13 +19,9 @@ class MainService : TileService() {
         val tm = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
         qsTile.label = tm.simOperatorName
         if (tm.dataState == 0) {
-            qsTile.icon = Icon.createWithResource(applicationContext, R.drawable.data_disable)
-            qsTile.updateTile()
             qsTile.state = Tile.STATE_ACTIVE
             qsTile.updateTile()
         } else {
-            qsTile.icon = Icon.createWithResource(applicationContext, R.drawable.data_enable)
-            qsTile.updateTile()
             qsTile.state = Tile.STATE_INACTIVE
             qsTile.updateTile()
         }
@@ -35,14 +30,10 @@ class MainService : TileService() {
     override fun onClick() {
         val tm = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
         if (tm.dataState == 0) {
-            qsTile.icon = Icon.createWithResource(applicationContext, R.drawable.data_enable)
-            qsTile.updateTile()
             ShellUtils.execCommand("svc data enable", true)
             qsTile.state = Tile.STATE_INACTIVE
             qsTile.updateTile()
         } else {
-            qsTile.icon = Icon.createWithResource(applicationContext, R.drawable.data_disable)
-            qsTile.updateTile()
             ShellUtils.execCommand("svc data disable", true)
             qsTile.state = Tile.STATE_ACTIVE
             qsTile.updateTile()
@@ -52,13 +43,9 @@ class MainService : TileService() {
     override fun onStartListening() {
         val tm = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
         if (tm.dataState == 0) {
-            qsTile.icon = Icon.createWithResource(applicationContext, R.drawable.data_disable)
-            qsTile.updateTile()
             qsTile.state = Tile.STATE_ACTIVE
             qsTile.updateTile()
         } else {
-            qsTile.icon = Icon.createWithResource(applicationContext, R.drawable.data_enable)
-            qsTile.updateTile()
             qsTile.state = Tile.STATE_INACTIVE
             qsTile.updateTile()
         }
